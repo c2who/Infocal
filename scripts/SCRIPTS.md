9611746964541f699904f471be624bda9e4e36c3
Jay Jones <blueacorn@users.noreply.github.com>
2025-08-07 17:00:33 -0600

    add: makedevices script to document device specs
    Collects device specs from SDK Device compiler.json files and creates easy reference outptu

---
+++ scripts/SCRIPTS.md
@@ -0,0 +1,8 @@
+# Support Scripts
+
+## makedevices
+Script to collect all the device information from the Garmin Connect IQ SDK 
+Devices folder and present it in a human-readable format.
+
+Usage Mac :
+  ./makedevices.sh ~/Library/Application\ Support/Garmin/ConnectIQ/Devices/
---
+++ scripts/makedevices.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script to collect all the device information from the Garmin Connect IQ SDK Devices folder
+# and present it in a human-readable CSV format.
+
+# Usage Mac :
+# ./makedevices.sh ~/Library/Application\ Support/Garmin/ConnectIQ/Devices/
+
+
+# Collect all compiler.json files from subdirectories and combines them into a single devices.json file.
+jq --slurp '.' "${1}"*/compiler.json  > devices.json
+
+# Convert the json file to csv
+(echo "deviceFamily,deviceId,displayType,connectIQVersion(s),audioMemoryLimit,backgroundMemoryLimit,datafieldMemoryLimit,glanceMemoryLimit,watchAppMemoryLimit,watchFaceMemoryLimit,partNumber(s),displayName"; \
+jq -r '.[] | [
+    .deviceFamily, 
+    .deviceId, 
+    .displayType,
+    ([.partNumbers[]? | select(.connectIQVersion) | .connectIQVersion] | unique | join(" ") // "-"),
+    (.appTypes[]? | select(.type=="audioContentProvider") | .memoryLimit) // "-",
+    (.appTypes[]? | select(.type=="background") | .memoryLimit) // "-",
+    (.appTypes[]? | select(.type=="datafield") | .memoryLimit) // "-",
+    (.appTypes[]? | select(.type=="glance") | .memoryLimit) // "-",
+    (.appTypes[]? | select(.type=="watchApp") | .memoryLimit) // "-",
+    (.appTypes[]? | select(.type=="watchFace") | .memoryLimit) // "-",
+    ([.partNumbers[]? | .number] | unique | join(" ") // "-"),
+    .displayName
+] | @csv' devices.json 
+) > devices.csv
